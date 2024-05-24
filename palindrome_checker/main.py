4a8c132d727ad9245338377836417b97ea636ff1
thekitsunedev <kisbendeguz2007@gmail.com>
2024-05-24 16:26:51 +0200

    Updated palindrome_checker/main.py

--- palindrome_checker/main.py
+++ palindrome_checker/main.py
@@ -1,4 +1,9 @@
 with open('./input.txt', 'r') as f:
-  input = f.read()
-
-print(input)
+    for line in f:
+        line: str = line.strip().lower()
+        inverted_line: str = line[::-1]
+        characters: set = {character for character in line}
+        if line == inverted_line:
+            print(f"YES, {len(characters)}")
+        else:
+            print("NO, -1")
