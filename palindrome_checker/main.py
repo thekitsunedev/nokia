19078359092b5a9a237d7868d62c57665399346e
thekitsunedev <kisbendeguz2007@gmail.com>
2024-05-24 18:04:05 +0200

    fixed:   palindrome_checker/main.py

--- palindrome_checker/main.py
+++ palindrome_checker/main.py
@@ -1,6 +1,6 @@
 with open('./input.txt', 'r') as f:
     for line in f:
-        line: str = line.strip().lower()
+        line: str = ''.join(char for char in line.strip().lower() if char.isalnum())
         inverted_line: str = line[::-1]
         characters: set = {character for character in line}
         if line == inverted_line:
